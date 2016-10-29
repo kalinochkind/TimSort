ced7028fc6bc1573412163f1b2ec12be75269195
Denis Kalinochkin <kalinochkind@gmail.com>
2016-10-29 16:33:24 +0300

    Generator upgrade

--- gen.py
+++ gen.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 import re
+import sys
 
-main = open('main.cpp').read()
+if len(sys.argv) < 2:
+    print('Usage: gen.py FILENAME')
+    sys.exit(1)
+try:
+    main = open(sys.argv[1]).read()
+except FileNotFoundError:
+    print('File {} not found'.format(sys.argv[1]))
+    sys.exit(2)
 repl = re.compile(r'#include "([a-z_]+\.h)"')
 while True:
     old = main
